38a803b76ced94f63a565214fc424045b0fb784e
thibaudcathala <thibaud.cathala@epitech.eu>
2024-03-25 10:37:52 +0100

    add: command list header

---
+++ include/command_list.h
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2024
+** B-NWP-400-PAR-4-1-myteams-thibaud.cathala
+** File description:
+** command_list
+*/
+
+#ifndef COMMAND_LIST_H_
+    #define COMMAND_LIST_H_
+
+typedef enum {
+    HELP,
+    LOGIN,
+    LOGOUT,
+    USERS,
+    USER,
+    SEND,
+    MESSAGES,
+    SUBSCRIBE,
+    SUBSCRIBED,
+    UNSUBSCRIBE,
+    USE_TEAMS_UUID,
+    USE_CHANNEL_UUID,
+    USE_THREAD_UUID,
+    CREATE_NEW_TEAM,
+    CREATE_NEW_CHANNEL,
+    CREATE_NEW_THREAD,
+    CREATE_NEW_REPLY,
+    LIST_TEAM,
+    LIST_CHANNEL,
+    LIST_THREAD,
+    LIST_REPLY,
+    INFO_LOGGED_IN_USER,
+    INFO_CURRENT_TEAM_DETAILS,
+    INFO_CURRENT_CHANNEL_DETAILS,
+    INFO_CURRENT_THREAD_DETAILS
+} command_e;
+
+#endif /* !COMMAND_LIST_H_ */
