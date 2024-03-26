7b91e9800e8c0f2733965484a59bdb669374c6f7
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-03-26 17:03:49 +0100

    add: new method for parse the input of the client, we first clean of the unused space out of the quotes and after cut by quotes for have a clean array of all good arguments

--- my_teams_client/src/execute_command.c
+++ my_teams_client/src/execute_command.c
@@ -7,11 +7,42 @@
 
 #include "myteams_client.h"
 
+char *remove_space_out_quotes(const char *string)
+{
+    bool is_in_quote = false;
+    size_t index = 0;
+    char *result = NULL;
+
+    result = malloc(sizeof(char) * (strlen(string)) + 1);
+    if (result == NULL) {
+        return NULL;
+    }
+    for (int i = 0; string[i] != '\0'; ++i) {
+        if (string[i] == '\"') {
+            is_in_quote = !is_in_quote;
+        }
+        if (!is_in_quote && string[i] == ' ') {
+            continue;
+        } else {
+            result[index] = string[i];
+            ++index;
+        }
+    }
+    return result;
+}
+
 void execute_command(client_t *client, char *command)
 {
-    /*
-    TODO: format the command to send to the server
-    */
+    const char *new_str = remove_space_out_quotes(command);
+    const char *delimiter = "\"\n";
+    int nb_word = count_nb_word(new_str, delimiter);
+    int *size_word = count_size_word(new_str, delimiter, nb_word);
+    char **array = my_str_to_word(new_str, delimiter, nb_word, size_word);
+
+    for (int i = 0; array[i] != NULL; ++i) {
+        printf("String : [%s]\n", array[i]);
+    }
+
     if (send(client->fd, command, strlen(command), 0) == -1) {
         exit_client(84, RED("Fail to send message to server.\n"));
     }
