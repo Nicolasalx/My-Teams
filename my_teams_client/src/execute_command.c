4b1eaa675775ff660c4f2663b23943cc83f15680
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-03-28 22:04:40 +0100

    mod: refactor the function execute_command and add a new function to send_cmd

--- my_teams_client/src/execute_command.c
+++ my_teams_client/src/execute_command.c
@@ -8,7 +8,7 @@
 #include "myteams_client.h"
 #include "command_list.h"
 
-void remove_space_opt(char c, bool *is_in_quote, char *result, size_t *index)
+static void remove_space_opt(char c, bool *is_in_quote, char *result, size_t *index)
 {
     if (!*is_in_quote && c == ' ') {
         return;
@@ -18,7 +18,7 @@ void remove_space_opt(char c, bool *is_in_quote, char *result, size_t *index)
     }
 }
 
-char *remove_space_out_quotes(const char *string)
+static char *remove_space_out_quotes(const char *string)
 {
     bool is_in_quote = false;
     size_t index = 0;
@@ -39,7 +39,7 @@ char *remove_space_out_quotes(const char *string)
     return result;
 }
 
-command_type_e parse_line(int *nb_word, char ***array, char *command)
+static command_type_e parse_line(int *nb_word, char ***array, char *command)
 {
     const char *new_str = remove_space_out_quotes(command);
     const char *delimiter = "\"\n";
@@ -59,6 +59,28 @@ command_type_e parse_line(int *nb_word, char ***array, char *command)
     return COMMAND_SUCCEED;
 }
 
+static command_type_e check_error_cmd(bool is_a_command, command_type_e command_type)
+{
+    if (is_a_command == false) {
+        printf("Command not recognized !\n");
+        return COMMAND_FAILED;
+    }
+    if (command_type == COMMAND_FAILED) {
+        return COMMAND_FAILED;
+    }
+    return COMMAND_SUCCEED;
+}
+
+static void send_cmd(client_t *client, bool is_a_command, command_type_e command_type, cmd_data_t *cmd_data)
+{
+    if (check_error_cmd(is_a_command, command_type) == COMMAND_FAILED) {
+        return;
+    }
+    if (send(client->fd, cmd_data, sizeof(cmd_data_t), 0) == -1) {
+        exit_client(84, RED("Fail to send message to server.\n"));
+    }
+}
+
 void execute_command(client_t *client, char *command)
 {
     cmd_data_t cmd_data = {0};
@@ -76,14 +98,5 @@ void execute_command(client_t *client, char *command)
             is_a_command = true;
         }
     }
-    if (is_a_command == false) {
-        printf("Command not recognized !\n");
-        return;
-    }
-    if (command_type == COMMAND_FAILED) {
-        return;
-    }
-    if (send(client->fd, &cmd_data, sizeof(cmd_data_t), 0) == -1) {
-        exit_client(84, RED("Fail to send message to server.\n"));
-    }
+    send_cmd(client, is_a_command, command_type, &cmd_data);
 }
