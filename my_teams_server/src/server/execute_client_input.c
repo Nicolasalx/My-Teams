7bc64fa8b339301e0ba9139c78f478829c20ac49
thibaudcathala <thibaud.cathala@epitech.eu>
2024-03-30 18:37:52 +0100

    fix: error in makefile where the server was also compiling some client file

--- my_teams_server/src/server/execute_client_input.c
+++ my_teams_server/src/server/execute_client_input.c
@@ -10,9 +10,6 @@
 
 void execute_client_input(server_t *server, client_t *client, cmd_data_t *cmd_data)
 {
-    if (cmd_data->type < NUMBER_CMD) {
-        printf("Received: %s\n", command_list[cmd_data->type].name);
-    }
     for (size_t i = 0; i < NUMBER_CMD; ++i) {
         if (cmd_data->type == i && cmd_handler[i]) {
             cmd_handler[i](server, client, cmd_data);
