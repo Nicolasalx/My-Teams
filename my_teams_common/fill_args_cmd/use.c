83c7bd35b4d329b46a42fb05e0dea20344a6559f
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-03-30 17:52:05 +0100

    mod: number of arguments in the function use if his equal to 1, the context is now set to NO_CONTEXT and the type is set to USE

--- my_teams_common/fill_args_cmd/use.c
+++ my_teams_common/fill_args_cmd/use.c
@@ -10,8 +10,14 @@
 
 command_type_e init_use(char **array, int nb_arg, cmd_data_t *cmd_data, command_e)
 {
-    if (user_is_login() == NOT_LOGGED || !(nb_arg >= 2 && nb_arg <= 4)) {
+    if (user_is_login() == NOT_LOGGED || !(nb_arg >= 1 && nb_arg <= 4)) {
         return COMMAND_FAILED;
+    }
+    if (nb_arg == 1) {
+        cmd_data->type = USE;
+        context = NO_CONTEXT;
+        cmd_data->arg4.nb_arg = 0;
+        return COMMAND_SUCCEED;
     }
     for (int i = 1; i <= nb_arg; i++) {
         if (strlen(array[i]) != UUID_LENGTH) {
