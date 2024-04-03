c1f276428170eb091d664432c055eb45ea71d204
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-04-03 20:49:56 +0200

    mod: system of parsing of the arguments have totally changed, now we have a strong system who parse elements between quotes even if the argument is an empty string

--- my_teams_client/src/execute_command.c
+++ my_teams_client/src/execute_command.c
@@ -8,79 +8,129 @@
 #include "myteams_client.h"
 #include "command_list.h"
 
-static void remove_space_opt(char c, bool is_in_quote, char *result, size_t *index)
-{
-    if (!is_in_quote && c == ' ') {
-        return;
-    } else {
-        result[*index] = c;
-        ++*index;
-    }
-}
-
-static bool is_a_delim(const char *delimiter, char c)
-{
-    for (int i = 0; delimiter[i] != '\0'; ++i) {
-        if (delimiter[i] == c) {
-            return true;
-        }
-    }
-    return false;
-}
-
-static char *remove_space_out_quotes(const char *string, size_t *nb_quotes)
-{
-    bool is_in_quote = false;
-    size_t index = 0;
-    char *result = NULL;
-
-    result = my_calloc(sizeof(char) * (strlen(string)) + 1);
-    for (int i = 0; string[i] != '\0'; ++i) {
-        if (string[i] == '\"') {
-            is_in_quote = !is_in_quote;
-            ++*nb_quotes;
-        } else if (*nb_quotes > 0 && is_in_quote == false && !is_a_delim("\"\n \t", string[i])) {
-            return NULL;
-        }
-        remove_space_opt(string[i], is_in_quote, result, &index);
-    }
-    if (*nb_quotes % 2 != 0) {
-        return NULL;
-    }
-    return result;
-}
-
-static command_type_e check_nb_arg(int *nb_word, size_t nb_quotes)
+static command_type_e check_nb_arg(int *nb_word)
 {
     if (*nb_word < 1 || *nb_word > 4) {
         printf("Command not recognized !\n");
         return COMMAND_FAILED;
     }
+    return COMMAND_SUCCEED;
+}
 
-    if (*nb_word == 1 && nb_quotes == 0) {
-        return COMMAND_SUCCEED;
-    } else if (*nb_word > 1 && nb_quotes == (size_t)(*nb_word - 1) * 2) {
-        return COMMAND_SUCCEED;
+static int count_nb_word_quotes(const char *str)
+{
+    int result = 0;
+    bool is_in_quote = false;
+
+    for (size_t i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == '\"') {
+            is_in_quote = !is_in_quote;
+            ++result;
+        }
     }
-    printf("Command not recognized !\n");
-    return COMMAND_FAILED;
+    if (result % 2 != 0) {
+        return -1;
+    }
+    return result / 2;
+}
+
+static void count_size_word_opt(bool *is_in_quotes, int **size_word, int *index, int *count_size_word)
+{
+    if (*is_in_quotes) {
+        (*size_word)[*index] = *count_size_word;
+        *is_in_quotes = false;
+        ++*index;
+        *count_size_word = 0;
+    } else {
+        *is_in_quotes = true;
+    }
+}
+
+static int *count_size_word_quotes(int nb_word, char *delimiter, const char *str)
+{
+    int *size_word = malloc_array(nb_word);
+    int count_size_word = 0;
+    int index = 0;
+    bool is_in_quotes = false;
+
+    for (size_t i = 0; str[i] != '\0'; ++i) {
+        if (is_in_str(str[i], delimiter)) {
+            count_size_word_opt(&is_in_quotes, &size_word, &index, &count_size_word);
+        } else if (is_in_quotes) {
+            ++count_size_word;
+        }
+    }
+    return size_word;
+}
+
+static void my_str_to_word_opt(bool *is_in_quotes, int *i_index, int *j_index)
+{
+    if (*is_in_quotes) {
+        ++*i_index;
+        *j_index = 0;
+        *is_in_quotes = false;
+    } else {
+        *is_in_quotes = true;
+    }
+}
+
+static char **my_str_to_word_quotes(int nb_word, int *size_word,
+    char *delimiter, const char *str)
+{
+    char **word = malloc_adv_board(nb_word, size_word);
+    int i_index = 0;
+    int j_index = 0;
+    bool is_in_quotes = false;
+
+    for (size_t i = 0; str[i] != '\0'; ++i) {
+        if (is_in_str(str[i], delimiter)) {
+            my_str_to_word_opt(&is_in_quotes, &i_index, &j_index);
+        } else if (is_in_quotes) {
+            word[i_index][j_index] = str[i];
+            ++j_index;
+        }
+    }
+    return word;
+}
+
+static char *get_first_command(const char *str, const char *delimiter)
+{
+    size_t idx = 0;
+    char *result;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (is_in_str(str[i], delimiter)) {
+            break;
+        }
+        ++idx;
+    }
+    result = my_malloc(sizeof(char) * (idx + 1));
+    memcpy(result, str, idx);
+    return result;
 }
 
 static command_type_e parse_line(int *nb_word, char ***array, char *command)
 {
-    size_t nb_quotes = 0;
-    const char *new_str = remove_space_out_quotes(command, &nb_quotes);
-    const char *delimiter = "\"\n";
-    int *size_word = NULL;
+    int *size_word_quotes = NULL;
+    char **board;
+    char *frst_command = get_first_command(command, "\" \n\t");
 
-    if (new_str == NULL) {
+    *nb_word = count_nb_word_quotes(command);
+    if (*nb_word == -1) {
         printf("Command not recognized !\n");
         return COMMAND_FAILED;
     }
-    *nb_word = count_nb_word(new_str, delimiter);
-    size_word = count_size_word(new_str, delimiter, *nb_word);
-    *array = my_str_to_word(new_str, delimiter, *nb_word, size_word);
-    return check_nb_arg(nb_word, nb_quotes);
+    size_word_quotes = count_size_word_quotes(*nb_word, "\"\n", command);
+    *array = my_str_to_word_quotes(*nb_word, size_word_quotes, "\"\n", command);
+    ++*nb_word;
+    board = my_malloc(sizeof(char *) * (*nb_word + 1));
+    board[0] = frst_command;
+    for (int i = 0; i < *nb_word - 1; i++) {
+        board[i + 1] = (*array)[i];
+    }
+    board[*nb_word] = NULL;
+    *array = board;
+    return check_nb_arg(nb_word);
 }
 
 static command_type_e check_error_cmd(bool is_a_command, command_type_e command_type)
@@ -95,7 +145,8 @@ static command_type_e check_error_cmd(bool is_a_command, command_type_e command_
     return COMMAND_SUCCEED;
 }
 
-static void send_cmd(client_t *client, bool is_a_command, command_type_e command_type, cmd_data_t *cmd_data)
+static void send_cmd(client_t *client, bool is_a_command,
+    command_type_e command_type, cmd_data_t *cmd_data)
 {
     if (check_error_cmd(is_a_command, command_type) == COMMAND_FAILED) {
         return;
@@ -118,7 +169,8 @@ void execute_command(client_t *client, char *command)
     }
     for (int i = 0; i < NUMBER_CMD; ++i) {
         if (strcmp(command_list[i].name, array[0]) == 0) {
-            command_type = command_list[i].cmd_function(array, nb_word, &cmd_data, command_list[i].type);
+            command_type = command_list[i].cmd_function(array, nb_word,
+                &cmd_data, command_list[i].type);
             is_a_command = true;
         }
     }
